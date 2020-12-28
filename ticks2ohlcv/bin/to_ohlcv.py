414bca5f7bb85bc0c086a8225e1b764f3303752c
Luis Parravicini <lparravi@gmail.com>
2020-12-28 18:40:03 -0300

    bugfix: order of params

--- ticks2ohlcv/bin/to_ohlcv.py
+++ ticks2ohlcv/bin/to_ohlcv.py
@@ -16,5 +16,5 @@ data_out_path = sys.argv[3]
 
 print(f'parsing {data_in_path} in {interval}m frames')
 df = ticks.to_ohlcv(data_in_path, interval, verbose=True)
-ticks.export_dataframe(df, data_out_path)
+ticks.export_dataframe(data_out_path, df)
 print(f'exported parsed data to {data_out_path}')
