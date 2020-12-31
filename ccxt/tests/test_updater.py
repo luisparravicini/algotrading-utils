edc3833f87cd1b9d363274f70f8bb72f2928aefb
Luis Parravicini <lparravi@gmail.com>
2020-12-31 07:59:46 -0300

    testing fetch_ohlcv

--- ccxt/tests/test_updater.py
+++ ccxt/tests/test_updater.py
@@ -11,14 +11,20 @@ class MockExchange:
 
     def __init__(self, exchange_name):
         self.exchange_name = exchange_name
+        self.ohlcv_data = []
     
     def fetch_ohlcv(self, symbol, timeframe, limit):
-        raise ccxt.BadSymbol()
+        if symbol != MockExchange.VALID_SYMBOL:
+            raise ccxt.BadSymbol()
+        
+        return self.ohlcv_data
 
 
 @pytest.fixture
 def updater(tmp_path):
-    return Updater(MockExchange.VALID_NAME, MockExchange.VALID_SYMBOL, tmp_path)
+    updater = Updater(MockExchange.VALID_NAME, MockExchange.VALID_SYMBOL, tmp_path)
+    updater.exchange = MockExchange(updater.exchange.name)
+    return updater
 
 
 def test_unknown_exchange(tmp_path):
@@ -29,9 +35,8 @@ def test_unknown_exchange(tmp_path):
         Updater(name, MockExchange.VALID_SYMBOL, tmp_path)
 
 
-def test_unknown_symbol(tmp_path):
-    updater = Updater('kraken', MockExchange.INVALID_SYMBOL, tmp_path)
-    updater.exchange = MockExchange(updater.exchange.name)
+def test_unknown_symbol(updater):
+    updater.symbol = MockExchange.INVALID_SYMBOL
     with pytest.raises(ccxt.BadSymbol):
         updater.fetch_ohlcv()
 
@@ -47,3 +52,13 @@ def test_database_name(updater):
 
 def test_database_inside_db_path(updater):
     assert updater.db_path.parent == updater.db_base_path
+
+
+def test_fetch_ohlcv_discards_last(updater):
+    updater.exchange.ohlcv_data = [1, 2, 3]
+    assert updater.fetch_ohlcv() == [1, 2]
+
+
+def test_fetch_ohlcv_empty(updater):
+    updater.exchange.ohlcv_data = []
+    assert updater.fetch_ohlcv() == []
